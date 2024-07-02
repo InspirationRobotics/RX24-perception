f6d148e26271de9365d4dfb4d45dbf94a2326f1a
Eesh Vij <eeshvij@gmail.com>
2024-07-01 20:54:06 -0700

    First push, figuring things oout

---
+++ camera/take_pictures.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+from pathlib import Path
+
+
+cap = cv2.VideoCapture(0)
+
+CURRENT_FILE_PATH = Path(__file__).parent.absolute()
+IMG_FILE_PATH = CURRENT_FILE_PATH / "img"
+
+if not IMG_FILE_PATH.exists():
+    os.mkdir(IMG_FILE_PATH)
+
+cv2.namedWindow("Camera", cv2.WINDOW_NORMAL) 
+cv2.resizeWindow("Camera", 640, 480)
+
+i = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: failed to capture image")
+        continue
+
+    cv2.imshow("Camera", frame)
+    
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == ord('q'):
+        print("Exiting...")
+        break
+
+    elif k == ord('s'):
+        i += 1
+        file_name = f"image_{i}.jpg"
+        print(f'saving image: {file_name}')
+        cv2.imwrite(str(IMG_FILE_PATH / file_name), frame)
+
+    if i == 20:
+        print("Max number of images saved")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ camera/test_cv.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture("v4l2src ! image/jpeg, width=1920, height=1080,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw, format=BGR ! appsink ")
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: failed to capture image")
+        break
+
+    cv2.imshow("Camera", frame)
+    
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == ord('q'):
+        print("Exiting...")
+        break
