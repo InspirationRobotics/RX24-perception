6595dc6b1e986897295f926f8dce1ec3e1a7ae6b
Eesh Vij <eeshvij@gmail.com>
2024-07-08 08:46:44 -0700

    added frame counter to yolo

--- camera/dev/ML_testing/test_yolo.py
+++ camera/dev/ML_testing/test_yolo.py
@@ -1,21 +1,26 @@
 from ultralytics import YOLO
 import numpy as np
 import cv2
+import time
 
 # Load a YOLOv8n PyTorch model
 model = YOLO("yolov8n.pt")
 
 cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! image/jpeg, width=1920, height=1080,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw, format=BGR ! appsink ")
+#cap = cv2.VideoCapture(0) 
 
 cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Camera", 640, 480)
 
+pre_frame = 0
+post_frame = 0
+
 while True:
+    pre_frame = time.time()
     ret, frame = cap.read()
     if not ret:
         print("Error: failed to capture image")
         continue
-
     results = model.predict(frame)
     result = results[0]
     bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
@@ -24,6 +29,10 @@ while True:
         (x, y, x2, y2) = bbox
         cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
         cv2.putText(frame, result.names[cls], (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 225), 2)
+
+    post_frame = time.time()
+    fps = 1 / (post_frame - pre_frame)
+    cv2.putText(frame, f"FPS: {fps:.2f}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
