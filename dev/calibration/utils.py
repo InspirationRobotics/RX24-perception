9b3d646e9add8aa89fc75e006afcabfc0eb6cc48
crae6 <carae@ucsd.edu>
2024-09-23 21:20:34 -0700

    makes more user-friendly and adds detailed comments

--- dev/calibration/utils.py
+++ dev/calibration/utils.py
@@ -1,16 +1,17 @@
 import numpy as np
 import cv2
 
+"""
+Calculate the transformation matrix using the camera and LiDAR points.
+Parameters: 
+- camera_corners: (N, 2) array of camera corner points in image space (2D).
+- lidar_corners: (N, 3) array of LiDAR corner points in world space (3D).
+- camera_matrix: Camera intrinsic matrix.
+- dist_coeffs: Camera distortion coefficients.
+Returns: 
+- (4, 4) transformation matrix.
+"""
 def calculate_transformation(camera_corners, lidar_corners, camera_matrix, dist_coeffs):
-    """
-    Calculate the transformation matrix using the camera and LiDAR points.
-    
-    :param camera_corners: (N, 2) array of camera corner points in image space (2D).
-    :param lidar_corners: (N, 3) array of LiDAR corner points in world space (3D).
-    :param camera_matrix: Camera intrinsic matrix.
-    :param dist_coeffs: Camera distortion coefficients.
-    :return: (4, 4) transformation matrix.
-    """
     assert len(camera_corners) == len(lidar_corners), "Number of camera and LiDAR points must be the same"
     
     camera_corners = camera_corners.astype(np.float32)
