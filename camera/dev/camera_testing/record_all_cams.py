dd45ee0f947d47ab56be1265b4266ccaf2609dd2
Dat-Bois <eeshvij@gmail.com>
2024-11-03 18:35:56 -0800

    GPS+Cam test script

--- camera/dev/camera_testing/record_all_cams.py
+++ camera/dev/camera_testing/record_all_cams.py
@@ -24,19 +24,27 @@ def init_cameras(addrs : list, record : bool = True):
             writer_list.append(None)
     return camera_list, writer_list
 
-def run(record = True):
+def run(record = True, ser = None):
     camera_list, writer_list = init_cameras(camera_addrs, record)
     for camera in camera_list:
         cv2.namedWindow(f"{camera.camera_path}", cv2.WINDOW_NORMAL) 
         cv2.resizeWindow(f"{camera.camera_path}", 640, 480)
     while camera_list[0].stream:
+        last_time = time.time()
         for camera, writer in zip(camera_list, writer_list):
             frame : Image = camera.get_latest_frame()
             if frame is None: continue
             frame = frame.frame
+            # Draw timestamp on frame
+            curr_time = time.time()
+            cv2.putText(frame, f"{curr_time}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.imshow(f"{camera.camera_path}", frame)
             if writer is not None:
                 writer.write(frame)
+            if time.time() - last_time > 0.2 and ser is not None:
+                # heartbeat
+                ser.send("heartbeat")
+                last_time = time.time()
         if cv2.waitKey(4) & 0xFF == ord('q'):
             break
     for camera, writer in zip(camera_list, writer_list):
@@ -45,12 +53,36 @@ def run(record = True):
             writer.release()
         cv2.destroyAllWindows()
 
+def record_gps_data(record_gps : bool):
+    if not record_gps:
+        return None
+    
+    from comms_core import Server, CustomSocketMessage as csm
+    def log_data(data : str, addr : str):
+        data : dict = csm.decode(data)
+        if data.get("current_position") is not None:
+            with open("gps_data.txt", "a") as f:
+                f.write(f"{time.time()}_%_{data}\n")
+
+    ser = Server(default_callback=log_data)
+    ser.start()
+    return ser
+
 if __name__ == "__main__":
     record = input("Record? (y/n)")
     if record in ["y", "yes"]:
         print("Recording all cameras...")
         record = True
+        rec_gps = input("Record GPS data? (y/n)")
+        if rec_gps in ["y", "yes"]:
+            print("Recording GPS data...")
+            record_gps = True
+        else:
+            print("Not recording GPS data...")
+            record_gps = False
     else:
         print("Not recording...")
         record = False
-    run(record)
+        record_gps = False
+    server = record_gps_data(record_gps)
+    run(record, server)
